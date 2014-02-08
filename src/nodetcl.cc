7551bf208c7d0a664a5312b1b5f17ce75ee84634
Eric Gallager <egall@gwmail.gwu.edu>
2014-02-08 09:53:38 -0500

    misc. fixes
    still fails to link properly though

--- src/nodetcl.cc
+++ src/nodetcl.cc
@@ -1,4 +1,4 @@
-/*
+/* nodetcl.cc
  * NodeTcl is a native Node extension that embeds a Tcl interpreter
  * within the Node.JS environment, allowing you to invoke Tcl commands
  * from within JavaScript code.
@@ -8,8 +8,35 @@
  * Distributed under BSD license.  See LICENSE for details.
  */
 
-#include <v8.h>
-#include <node.h>
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#else
+# define NON_AUTOTOOLS_BUILD 1
+#endif /* HAVE_CONFIG_H */
+#ifdef NON_AUTOTOOLS_BUILD
+# include <v8.h>
+# include <node.h>
+#else
+# ifdef HAVE_V8_H
+#  include <v8.h>
+# else
+#  ifdef HAVE_NODE_V8_H
+#   include <node/v8.h>
+#  else
+#   warning nodetcl.cc expects a v8 header to be included.
+#  endif /* HAVE_NODE_V8_H */
+# endif /* HAVE_V8_H */
+# ifdef HAVE_NODE_H
+#  include <node.h>
+# else
+#  ifdef HAVE_NODE_NODE_H
+#   include <node/node.h>
+#  else
+#   warning nodetcl.cc expects a nodejs header to be included.
+#  endif /* HAVE_NODE_NODE_H */
+# endif /* HAVE_NODE_H */
+#endif /* NON_AUTOTOOLS_BUILD */
+
 #include <tcl.h>
 #include <string.h>
 #include <stdlib.h>
@@ -52,7 +79,7 @@ public:
   NodeTcl()
   {
     m_interp = Tcl_CreateInterp();
-    time_limit = 0; 
+    time_limit = 0;
   }
 
   ~NodeTcl()
@@ -78,9 +105,9 @@ public:
     hw->Wrap(args.This());
     return args.This();
   }
-  
+
   // ----------------------------------------------------
-  
+
   /*
    * MakeSafe() -- transforms interpreter into a safe interpreter
    */
@@ -88,15 +115,15 @@ public:
   {
     HandleScope scope;
     NodeTcl* hw = ObjectWrap::Unwrap<NodeTcl>(args.This());
-    
+
     int ret = Tcl_MakeSafe(hw->m_interp);
-    
+
     Local<Integer> result = Integer::New(ret);
     return scope.Close(result);
   }
-  
+
   // ----------------------------------------------------
-  
+
   /*
    * DeleteCommand(String) -- remove a command from the interpreter
    */
@@ -104,22 +131,22 @@ public:
   {
     HandleScope scope;
     NodeTcl* hw = ObjectWrap::Unwrap<NodeTcl>(args.This());
-    
+
     if (args.Length() != 1 || !args[0]->IsString())
       return ThrowException(Exception::TypeError(String::New("Argument must be a string")));
-    
+
     String::Utf8Value str(args[0]);
     const char* cstr = *str;
-    
+
     int ret = Tcl_DeleteCommand(hw->m_interp, cstr);
     ret++;
-    
+
     Local<Integer> result = Integer::New(ret);
     return scope.Close(result);
   }
-  
+
   // ----------------------------------------------------
-  
+
   /*
    * LastError() -- gets stacktrace for the last error that occurred
    */
@@ -127,7 +154,7 @@ public:
   {
     HandleScope scope;
     NodeTcl* hw = ObjectWrap::Unwrap<NodeTcl>(args.This());
-    
+
     Tcl_Obj *options = Tcl_GetReturnOptions(hw->m_interp, TCL_ERROR);
     Tcl_Obj *key = Tcl_NewStringObj("-errorinfo", -1);
     Tcl_Obj *stacktrace;
@@ -137,13 +164,13 @@ public:
     Tcl_DictObjGet(NULL, options, key, &stacktrace);
     Tcl_DecrRefCount(options);
     Tcl_DecrRefCount(key);
-    
+
     Local<String> result = String::New(Tcl_GetStringFromObj(stacktrace, NULL));
     return scope.Close(result);
   }
-  
+
   // ----------------------------------------------------
-  
+
   /*
    * SetTimeLimit(Integer) -- sets time limit for next Tcl command
    */
@@ -151,43 +178,43 @@ public:
   {
     HandleScope scope;
     NodeTcl* hw = ObjectWrap::Unwrap<NodeTcl>(args.This());
-    
+
     if (args.Length() != 1 || ! args[0]->IsUint32())
       return ThrowException(Exception::TypeError(String::New("Argument must be an integer")));
-    
+
     int limit = args[0]->ToInteger()->Value();
     hw->time_limit = limit;
-    
+
     return scope.Close(args[0]);
   }
-  
+
   // ----------------------------------------------------
-  
+
   static Handle<Value> GetTimeLimit(const Arguments& args)
   {
     HandleScope scope;
     NodeTcl* hw = ObjectWrap::Unwrap<NodeTcl>(args.This());
-    
+
     Local<Integer> result = Integer::New(hw->time_limit);
     return scope.Close(result);
   }
-  
+
   // ----------------------------------------------------
-  
+
   /*
    * converts JavaScript objects into Tcl objects (can handle arrays, strings,
    * numbers, booleans and simple key-value-mapping objects -- might die
    * horribly if fed with the wrong data types)
-   */   
+   */
   static Tcl_Obj* jsToTcl(Local<Value> var, Tcl_Interp* interp)
   {
     Tcl_Obj* result;
-    
-    
+
+
     if (var->IsArray()) {
       result = Tcl_NewListObj(0, NULL);
       Local<Array> arrvar = Local<Array>::Cast(var);
-      
+
       for (unsigned int i = 0; i < arrvar->Length(); i++) {
         Tcl_ListObjAppendElement(
           interp,
@@ -195,20 +222,20 @@ public:
           jsToTcl(arrvar->Get(i), interp)
         );
       }
-      
-      
+
+
     } else if (var->IsBoolean()) {
       if (var->IsTrue())
         result = Tcl_NewBooleanObj(1);
       else
         result = Tcl_NewBooleanObj(0);
-      
-    
+
+
     } else if (var->IsObject()) {
       result = Tcl_NewDictObj();
       Local<Object> objvar = Local<Object>::Cast(var);
       Local<Array> keys = objvar->GetPropertyNames();
-      
+
       for (unsigned int i = 0; i < keys->Length(); i++) {
         Tcl_DictObjPut(
           interp,
@@ -217,19 +244,19 @@ public:
           jsToTcl(objvar->Get(keys->Get(i)), interp)
         );
       }
-    
-      
+
+
     } else {
       String::Utf8Value str(var);
       const char* cstr = *str;
       result = Tcl_NewStringObj(cstr, strlen(cstr));
     }
-    
+
     return result;
   }
 
   // ----------------------------------------------------
-  
+
   /*
    * converts a Tcl object into the corresponding JavaScript types
    */
@@ -239,90 +266,90 @@ public:
       Tcl_DictSearch search;
       Tcl_Obj *key, *value;
       int done;
-      
+
       Tcl_DictObjFirst(interp, obj, &search, &key, &value, &done);
       Local<Object> result = Object::New();
-      
+
       for (; !done; Tcl_DictObjNext(&search, &key, &value, &done)) {
         Handle<Value> x = String::New(Tcl_GetString(key));
         Handle<Value> y = tclToJs(value, interp);
         result->Set(x, y);
       }
-      
+
       Tcl_DictObjDone(&search);
-      
+
       return result;
-      
-      
+
+
     } else if (obj->typePtr && ! strcmp(obj->typePtr->name, "list")) {
       int objc;
       Tcl_Obj **objv;
-      
+
       Tcl_ListObjGetElements(interp, obj, &objc, &objv);
       Local<Array> result = Array::New(objc);
-      
+
       for (int i = 0; i < objc; i++) {
         Handle<Number> x = Number::New(i);
         Handle<Value>  y = tclToJs(objv[i], interp);
         result->Set(x, y);
       }
-      
+
       return result;
-    
-    
+
+
     } else if (obj->typePtr && ! strcmp(obj->typePtr->name, "int")) {
       long john;
       Tcl_GetLongFromObj(interp, obj, &john);
-      
+
       return Number::New(john);
-    
-    
+
+
     } else if (obj->typePtr && ! strcmp(obj->typePtr->name, "double")) {
       double cheese;
       Tcl_GetDoubleFromObj(interp, obj, &cheese);
-      
+
       return Number::New(cheese);
-      
-      
+
+
     } else {
       return String::New(Tcl_GetUnicode(obj));
     }
   }
-  
+
   // ----------------------------------------------------
-  
+
   /*
    * call(String, ...) -- calls a Tcl proc with some arguments
    */
   static Handle<Value> Call(const Arguments& args)
   {
     HandleScope scope;
-    
+
     if (args.Length() < 1 || !args[0]->IsString())
       return ThrowException(Exception::TypeError(String::New("Argument must be a string")));
-    
+
     NodeTcl* hw = ObjectWrap::Unwrap<NodeTcl>(args.This());
     Tcl_Obj** params = (Tcl_Obj**)malloc(args.Length() * sizeof(Tcl_Obj*));
     int i;
-    
+
     for (i = 0; i < args.Length(); i++) {
     Tcl_Obj* obj = jsToTcl(args[i], hw->m_interp);
     Tcl_IncrRefCount(obj);
       params[i] = obj;
     }
-    
+
     if (hw->time_limit) {
       Tcl_Time limit;
       Tcl_GetTime(&limit);
-      
+
       limit.sec += hw->time_limit;
-      
+
       Tcl_LimitTypeSet(hw->m_interp, TCL_LIMIT_TIME);
       Tcl_LimitSetTime(hw->m_interp, &limit);
     } else {
       Tcl_LimitTypeReset(hw->m_interp, TCL_LIMIT_TIME);
     }
-  
+
     int cc = Tcl_EvalObjv(hw->m_interp, args.Length(), params, 0);
     if (cc != TCL_OK) {
       for (i = 0; i < args.Length(); i++)
@@ -333,14 +360,14 @@ public:
     }
 
     Tcl_Obj *obj = Tcl_GetObjResult(hw->m_interp);
-  
+
     for (i = 0; i < args.Length(); i++)
       Tcl_DecrRefCount(params[i]);
     free(params);
-    
+
     return scope.Close(tclToJs(obj, hw->m_interp));
   }
-  
+
   // ----------------------------------------------------
 
   /*
@@ -357,19 +384,19 @@ public:
     Local<String> script = Local<String>::Cast(args[0]);
 
     NodeTcl* hw = ObjectWrap::Unwrap<NodeTcl>(args.This());
-    
+
     if (hw->time_limit) {
       Tcl_Time limit;
       Tcl_GetTime(&limit);
-      
+
       limit.sec += hw->time_limit;
-      
+
       Tcl_LimitTypeSet(hw->m_interp, TCL_LIMIT_TIME);
       Tcl_LimitSetTime(hw->m_interp, &limit);
     } else {
       Tcl_LimitTypeReset(hw->m_interp, TCL_LIMIT_TIME);
     }
-  
+
     int cc = Tcl_EvalEx(hw->m_interp, (const char*)*String::Utf8Value(script), -1, 0);
     if (cc != TCL_OK) {
       Tcl_Obj *obj = Tcl_GetObjResult(hw->m_interp);
@@ -377,7 +404,7 @@ public:
     }
 
     Tcl_Obj *obj = Tcl_GetObjResult(hw->m_interp);
-  
+
     return scope.Close(hw->tclToJs(obj, hw->m_interp));
   }
 
@@ -535,3 +562,5 @@ extern "C" {
 
   NODE_MODULE(nodetcl, init);
 }
+
+/* EOF */
